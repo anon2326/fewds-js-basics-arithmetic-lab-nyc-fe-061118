11f0d4124c0d95a8eb7f0211938a628c0850b5c5
Jeffrey Jiang <anon0503@gmail.com>
2018-07-18 16:24:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = oldID + 1000000000;
 var ageIsValid = Number.isInteger(currentAge);
 let randomNumber = (Math.random()*20) + 1;
 let randomInteger = Math.floor(randomNumber);
-let randomUserID = randomInteger + 100000000;
+let randomUserID = randomInteger + 1000000000;
